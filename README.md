cbfba414d31f2ad365af620c6bc0bac1b5121f94
tam2511 <tam2511@mail.ru>
2024-01-21 10:54:01 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pipez
+# Pipez - lightweight library for fast deploy stream handling
+
+## Install
+
+For installing default version of library use
+
+```
+pip install pipez
+```
+
+If you want install specific version pipez - use
+
+```
+pip install pipez[<your choice>]
+```
+
+Now available `...` versions.
---
+++ setup.py
@@ -0,0 +1,37 @@
+from itertools import chain
+
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+from pipez import __version__
+
+requires = [
+
+]
+
+extra_requires = {
+
+}
+
+extra_requires["all"] = list(
+    set(chain.from_iterable(extra_requires.values()))
+)
+
+setup(
+    name='pipez',
+    version=__version__,
+    author="Alexander Timofeev",
+    author_email="tam2511@mail.ru",
+    python_requires=">=3.7",
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tam2511/pipez",
+    install_requires=requires,
+    extras_require=extra_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+)
