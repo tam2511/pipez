a9dcc80b066f06b65a7336e480f9f4b0a73f0d56
purple63 <purple63@ya.ru>
2024-01-24 16:16:44 +0500

    v0.0.11

--- pipez/nodes/common/__init__.py
+++ pipez/nodes/common/__init__.py
@@ -1,2 +1,4 @@
 from pipez.nodes.common.group import Group
 from pipez.nodes.common.ungroup import Ungroup
+from pipez.nodes.common.get import Get
+
--- pipez/nodes/cv/__init__.py
+++ pipez/nodes/cv/__init__.py
@@ -1,1 +1,3 @@
 from pipez.nodes.cv.video_reader import VideoReader
+from pipez.nodes.cv.image_reader import ImageReader
+from pipez.nodes.cv.loop_video_reader import LoopVideoReader
