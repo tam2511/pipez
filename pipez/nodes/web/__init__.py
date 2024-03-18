db261056f538ca6c23192e3429f575076117d48e
purple63 <purple63@ya.ru>
2024-03-18 16:24:21 +0500

    fix

--- pipez/nodes/web/__init__.py
+++ pipez/nodes/web/__init__.py
@@ -1,2 +1,1 @@
 from pipez.nodes.web.fastapi_node import FastAPINode
-from pipez.nodes.web.video_test import TestVideoAPI
--- pipez/nodes/web/fastapi_node.py
+++ pipez/nodes/web/fastapi_node.py
@@ -1,53 +1,41 @@
-from typing import Optional, Callable, List
-
+from abc import ABC, abstractmethod
+from typing import Optional
 from fastapi import FastAPI, APIRouter
-from pydantic import BaseModel
 import uvicorn
 
 from pipez.node import Node
 from pipez.batch import Batch
 
 
-class FastAPINode(Node):
+class FastAPINode(Node, ABC):
     def __init__(
             self,
-            host: str = '127.0.0.1',
+            host: str = '0.0.0.0',
             port: int = 8888,
             **kwargs
     ):
         super().__init__(**kwargs)
-
         self._host = host
         self._port = port
-
         self._app = FastAPI()
         self._router = APIRouter()
         self._is_run = False
 
-    def inner_post_init(self):
+    def _run(self):
+        self.add_api_routes()
         self._app.include_router(self._router)
         uvicorn.run(self._app, host=self._host, port=self._port)
         self._is_run = True
-
-    def _add_route(
-            self,
-            rout_path: str,
-            end_point: Callable,
-            response_model: Optional[BaseModel],
-            methods: List[str]
-    ):
-        self._router.add_api_route(
-            rout_path,
-            end_point,
-            response_model=response_model,
-            methods=methods
-        )
 
     def work_func(
             self,
             data: Optional[Batch] = None
     ) -> Batch:
         if not self._is_run:
-            self.inner_post_init()
+            self._run()
 
         return Batch()
+
+    @abstractmethod
+    def add_api_routes(self):
+        pass
