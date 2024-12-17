bf230837eccae1ba35235ec8cd9028a68431d9ee
purple63 <purple63@ya.ru>
2024-12-17 19:23:26 +0500

    fix

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
+from os.path import dirname, join
+
+from setuptools import find_packages, setup
 
 
 setup(
     name='pipez',
-    version='0.0.130',
-    author="Alexander Timofeev",
-    author_email="tam2511@mail.ru",
-    python_requires=">=3.9",
-    packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/tam2511/pipez",
+    version='0.0.131',
+    python_requires='>=3.9',
     install_requires=[
-        'numpy',
-        'opencv-python',
         'fastapi',
-        'uvicorn',
-        'Jinja2'
+        'Jinja2',
+        'numpy',
+        'uvicorn'
     ],
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    author='Alexander Timofeev',
+    author_email='tam2511@mail.ru',
+    url='https://github.com/tam2511/pipez',
+    packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ],
+    license='GPLv3',
     include_package_data=True
 )
