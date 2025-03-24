8d4505b14a8efa75728cb2a93dcf655b83dcfc29
purple63 <purple63@ya.ru>
2025-03-24 23:32:49 +0500

    fix

--- pipez/nodes/node_fastapi.py
+++ pipez/nodes/node_fastapi.py
@@ -60,7 +60,13 @@ class NodeFastAPI(Node, ABC):
         @self.app.exception_handler(RequestValidationError)
         async def validation_exception_handler(request: Request, exc: RequestValidationError):
             logging.error(exc.errors())
-            return JSONResponse(dict(detail=json.dumps(exc.errors(), ensure_ascii=False)), 422)
+            errors = []
+
+            for error in exc.errors():
+                del error['ctx']
+                errors.append(error)
+
+            return JSONResponse(dict(detail=json.dumps(errors, ensure_ascii=False)), 422)
 
         @self.app.exception_handler(Exception)
         async def general_exception_handler(request: Request, exc: Exception):
