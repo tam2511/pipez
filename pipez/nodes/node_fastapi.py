a8fbcf8e47d4a4863efa11dc1944861ccc23124d
pure7000 <pure7000@ya.ru>
2025-10-09 10:34:03 +0500

    fix

--- pipez/nodes/node_fastapi.py
+++ pipez/nodes/node_fastapi.py
@@ -1,23 +1,14 @@
 import importlib.resources
-import json
-import logging
 from abc import ABC
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
 
 from ..core.batch import Batch
 from ..core.node import Node
-
-
-class DetailResponse(BaseModel):
-    detail: str
 
 
 class NodeFastAPI(Node, ABC):
@@ -30,10 +21,7 @@ class NodeFastAPI(Node, ABC):
         super().__init__(**kwargs)
         self.host = host
         self.port = port
-        self.app = FastAPI(docs_url=None,
-                           redoc_url=None,
-                           responses={422: dict(model=DetailResponse),
-                                      500: dict(model=DetailResponse)})
+        self.app = FastAPI(docs_url=None)
 
         self.app.mount('/static',
                        StaticFiles(directory=importlib.resources.files('pipez.resources') / 'fastapi'),
@@ -50,21 +38,6 @@ class NodeFastAPI(Node, ABC):
         @self.app.get(self.app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
         async def swagger_ui_redirect():
             return get_swagger_ui_oauth2_redirect_html()
-
-        @self.app.get('/redoc', include_in_schema=False)
-        async def redoc_html():
-            return get_redoc_html(openapi_url=self.app.openapi_url,
-                                  title=self.app.title,
-                                  redoc_js_url='/static/redoc.standalone.js')
-
-        @self.app.exception_handler(RequestValidationError)
-        async def validation_exception_handler(request: Request, exc: RequestValidationError):
-            logging.error(exc.errors())
-            return JSONResponse(dict(detail=json.dumps(exc.errors(), ensure_ascii=False)), 422)
-
-        @self.app.exception_handler(Exception)
-        async def general_exception_handler(request: Request, exc: Exception):
-            return JSONResponse(dict(detail=exc.__class__.__name__), 500)
 
     def processing(self, data: Optional[Batch]) -> Optional[Batch]:
         uvicorn.run(app=self.app,
