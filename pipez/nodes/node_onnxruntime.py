9b156a34bcc7c5e4c590c4e5add45201da68a3e7
pure7000 <pure7000@ya.ru>
2025-11-11 00:03:08 +0500

    fix

--- pipez/nodes/node_onnxruntime.py
+++ pipez/nodes/node_onnxruntime.py
@@ -12,18 +12,21 @@ class NodeONNXRuntime(Node, ABC):
     def __init__(
             self,
             model_path: str,
-            providers: List[Union[str, Tuple[str, Dict]]],
-            dtype: np.dtype = np.float32,
+            providers: List[Union[str, Tuple[str, Dict[str, Any]]]],
             **kwargs
     ):
         super().__init__(**kwargs)
         self.session = onnxruntime.InferenceSession(path_or_bytes=model_path, providers=providers)
-        self.dtype = dtype
         self.input_name = self.session.get_inputs()[0].name
         self.batch_size, self.channels, self.height, self.width = self.session.get_inputs()[0].shape
 
-        if not isinstance(self.batch_size, int) or self.batch_size < 1:
+        if not isinstance(self.batch_size, int):
             self.batch_size = 32
+
+        self.dtype = self.session.get_inputs()[0].type
+
+        if self.dtype == 'tensor(float)':
+            self.dtype = np.float32
 
         self.batch = np.zeros((self.batch_size, self.channels, self.height, self.width), dtype=self.dtype)
 
